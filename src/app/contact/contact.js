fc3dc5d731349375d6e5dac0be3d09a2b6622e86
CptDoraemon <yxx0827@gmail.com>
2018-12-14 03:22:25 -0500

    snow added in contact

--- src/app/contact/contact.js
+++ src/app/contact/contact.js
@@ -33,9 +33,14 @@ class Contact extends React.Component {
                 nameClassName: 'contact-form-name contact-form-error',
             });
         }
-        if(this.state.email === '' || this.state.email === 'Your Email please' || this.state.email.indexOf('@') === -1) {
+        if(this.state.email === '' || this.state.email === 'Your Email please') {
             this.setState({
                 email: 'Your Email please',
+                emailClassName: 'contact-form-email contact-form-error',
+            });
+        } else if(this.state.email.indexOf('@') === -1) {
+            this.setState({
+                email: 'Email invalid',
                 emailClassName: 'contact-form-email contact-form-error',
             });
         }
@@ -58,13 +63,66 @@ class Contact extends React.Component {
     handleMessageChange(e) {
         this.setState({message: e.target.value});
     }
+    letItSnow() {
+        let display = document.getElementById('display');
+        let ctx = display.getContext('2d');
+        let width = display.width = window.innerWidth;
+        let height = display.height = window.innerHeight;
+
+
+        class Snow {
+            constructor(){
+                this.x = Math.floor(Math.random() * width);
+                this.y = 0;
+                this.update = this.update.bind(this);
+                this.draw = this.draw.bind(this);
+                this.radius = Math.random() * 2 + 3;
+                this.speed = this.radius / 5;
+            }
+            draw() {
+                let x = this.x;
+                let y = this.y;
+                ctx.save();
+                ctx.fillStyle = 'rgb(255, 255, 255)';
+                ctx.beginPath();
+                ctx.arc(x, y, this.radius, 0, 2*Math.PI); // x, y, r, startAngle, endAngle
+                ctx.fill();
+                ctx.restore();
+            }
+            update() {
+                if(this.y <= height){
+                    this.y += this.speed;
+                }
+            }
+        }
+
+        let array = [];
+        requestAnimationFrame(frame);
+        function frame() {
+            requestAnimationFrame(frame);
+            if (Math.random() > 0.95) {
+                let snow = new Snow();
+                array.push(snow);
+            }
+            ctx.clearRect(0, 0, width, height);
+            array.map((i) => i.update());
+            array.map((i) => i.draw());
+            if(array.length > 100){
+                array.shift();
+            }
+        }
+    }
+    componentDidMount() {
+        if (window.innerWidth > 800) this.letItSnow();
+    }
 
     render() {
         return(
             <div className='contact-wrapper'>
+                <canvas id='display' width='1' height='1' />
                 <Link to='/home'>
                     <div className='contact-close-icon'>
-                        <IoIosClose size='3em'/>
+                        <IoIosClose size='4em'/>
                     </div>
                 </Link>
                 <form className='contact-form' onSubmit={this.handleSubmit} >
